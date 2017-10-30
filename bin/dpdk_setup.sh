34b1e0a5b58ab4f582f6f4d56dcec31222dbbe53
Peter Schmidt <peter@peterjs.com>
2017-10-30 18:28:14 +1100

    :memo: Add closer to working bash scripts
    Not verified from scratch, but a lot closer as the following commands now complete without errors:
    cd megakv/src
    RTE_SDK="$HOME/build/dpdk" make
    
    Some errors along the way:
    # TODO: Getting error with DPDK v16.11, trying v17.08 now
    #ubuntu@ip-zzz-zzz-zzz-zzz:~/src/megakv/src$ RTE_SDK="$HOME/build/dpdk" make
    #/home/ubuntu/build/dpdk/mk/internal/rte.extvars.mk:57: *** Cannot find .config in /home/ubuntu/build/dpdk/x86_64-native-linuxapp-gcc.  Stop.
    
    # After resolving the RTE_TARGET issue, I get:
    #dpdk.o: In function `mega_init_dpdk':
    #/home/ubuntu/src/megakv/src/dpdk.c:166: undefined reference to `rte_eal_pci_probe'
    #/home/ubuntu/src/megakv/src/../libgpuhash/lib/libgpuhash.a(gpu_hash.o):(.eh_frame+0x6b): undefined reference to `__gxx_personality_v0'
    #collect2: error: ld returned 1 exit status
    #/home/ubuntu/build/dpdk/mk/rte.app.mk:280: recipe for target 'megakv' failed
    #make[1]: *** [megakv] Error 1
    #/home/ubuntu/build/dpdk/mk/rte.extapp.mk:42: recipe for target 'all' failed
    #make: *** [all] Error 2

---
+++ bin/dpdk_setup.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Intel Data Plane Development Kit(DPDK) - Following this guide
+# https://www.slideshare.net/hisaki/intel-dpdk-step-by-step-instructions
+
+# 1. Combine several approaches to enable HugePages
+# https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/5/html/Tuning_and_Optimizing_Red_Hat_Enterprise_Linux_for_Oracle_9i_and_10g_Databases/sect-Oracle_9i_and_10g_Tuning_Guide-Large_Memory_Optimization_Big_Pages_and_Huge_Pages-Configuring_Huge_Pages_in_Red_Hat_Enterprise_Linux_4_or_5.html
+# https://askubuntu.com/questions/776929/how-to-edit-my-etc-sysctl-conf-file
+sudo mkdir /hugepages
+sudo sysctl -w vm.nr_hugepages=256
+# TODO: This is not idempotent yet, needs a guard
+echo "" | sudo tee -a /etc/sysctl.conf
+echo "# Enable hugepages for Intel DPDK" | sudo tee -a /etc/sysctl.conf
+echo "vm.nr_hugepages=256" | sudo tee -a /etc/sysctl.conf
+cat /etc/fstab
+# Should now report 256
+grep HugePages_Total /proc/meminfo
+
+# https://github.com/att/vfd/wiki/Building-the-igb_uio-driver
+sudo apt-get update
+sudo apt-get install gcc git gmake linux-headers-generic -y
+# Red Hat version...
+#sudo yum install gcc git gmake kernel-devel -y
+
+mkdir $HOME/build
+cd $HOME/build
+git clone http://dpdk.org/git/dpdk
+cd dpdk
+# TODO: This is not v1.7.1 the MegaKV author recommended - will it work?
+# Note that v1.7.1 gives a bunch of errors like:
+# /home/ubuntu/build/dpdk/mk/toolchain/gcc/rte.toolchain-compat.mk:46: You are not using GCC 4.x. This is neither supported, nor tested.
+# /usr/src/linux-headers-4.4.0-1022-aws/Makefile:1420: recipe for target '_module_/home/ubuntu/build/dpdk/build/build/lib/librte_eal/linuxapp/igb_uio' failed
+#
+# A list of DPDK versions is available at:
+# http://dpdk.org/doc/guides/rel_notes/
+#
+# See also the "Build Directory Concept"
+# http://dpdk.org/doc/guides/prog_guide/dev_kit_build_system.html
+# Note: Using v16.11 over v17.08 as the former felt like it compiled more quickly
+#
+# v16.11 timing:
+# Build complete [x86_64-native-linuxapp-gcc]
+# 134.28user 15.80system 2:54.72elapsed 85%CPU (0avgtext+0avgdata 176284maxresident)k
+# 0inputs+234104outputs (0major+10168375minor)pagefaults 0swaps
+#
+# v17.08 timing?
+# Build complete [x86_64-native-linuxapp-gcc]
+# 144.03user 19.50system 3:16.87elapsed 83%CPU (0avgtext+0avgdata 167300maxresident)k
+# 0inputs+247560outputs (0major+12937461minor)pagefaults 0swaps
+git checkout v16.11
+
+# Define RTE_TARGET to work with MegaKV's defaults
+RTE_TARGET="x86_64-native-linuxapp-gcc"
+make config T=x86_64-native-linuxapp-gcc O=${RTE_TARGET}
+
+# Current working directory should be $HOME/build/dpdk, so downstream tools
+# can use RTE_SDK="$HOME/build/dpdk"
+echo `pwd`
+
+# This part takes about 2-5 minutes to compile stuff
+RTE_SDK=`pwd` make O=${RTE_TARGET}
+
+echo "\e[32m Yay - DPDK installation should have completed successfully. \e[0m"
+echo "\e[32m You should now be able to use `RTE_SDK="$HOME/build/dpdk"` \e[0m"
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,52 +1,39 @@
 #!/usr/bin/env bash
 
-wget http://kay21s.github.io/megakv/megakv-0.1-alpha.tar.gz
-tar -xvf megakv-0.1-alpha.tar.gz
+# Run on an AWS p2.xlarge instance with the ami-5af95d20,
+# "Deep Learning AMI CUDA 9 Ubuntu Version".
+#
+# On instance login, it reports:
+# > Welcome to Ubuntu 16.04.3 LTS (GNU/Linux 4.4.0-1022-aws x86_64)
+#
+# ubuntu@ip-zzz-zzz-zzz-zzz:~/src$ ls
+# anaconda2  caffe2                      tensorflow_anaconda2  tensorflow_python3
+# anaconda3  caffe2_anaconda2  mxnet     tensorflow_anaconda3
+# bin        logs              OpenBLAS  tensorflow_python2
+#
+# See https://aws.amazon.com/marketplace/pp/B076TGJHY1
+#
+# This script assumes the following manual setup has been completed:
+#
+#     sudo apt-get install git
+#     cd $HOME
+#     git clone https://github.com/pzrq/megakv.git megakv
 
-# Intel DPDK Setup - Following this guide
-# https://www.slideshare.net/hisaki/intel-dpdk-step-by-step-instructions
+# Change if you cloned the repo somewhere else - YMMV (PRs welcome)
+MEGAKV_HOME="$HOME/megakv"
 
-# 1. Combine several approaches to enable HugePages
-# https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/5/html/Tuning_and_Optimizing_Red_Hat_Enterprise_Linux_for_Oracle_9i_and_10g_Databases/sect-Oracle_9i_and_10g_Tuning_Guide-Large_Memory_Optimization_Big_Pages_and_Huge_Pages-Configuring_Huge_Pages_in_Red_Hat_Enterprise_Linux_4_or_5.html
-# https://askubuntu.com/questions/776929/how-to-edit-my-etc-sysctl-conf-file
-sudo mkdir /hugepages
-sudo sysctl -w vm.nr_hugepages=256
-# TODO: This is not idempotent yet, needs a guard
-echo "" | sudo tee -a /etc/sysctl.conf
-echo "# Enable hugepages for Intel DPDK" | sudo tee -a /etc/sysctl.conf
-echo "vm.nr_hugepages=256" | sudo tee -a /etc/sysctl.conf
-cat /etc/fstab
-# Should now report 256
-grep HugePages_Total /proc/meminfo
+# TODO: Step 0 - Possibly fix some problems with the AMI (do we need this?)
+apt-get -f install
+apt-get install libdpdk0 libxenstore3.0 dpdk
 
-# Note 1.8, not 1.7.1 as recommended in README
-#wget http://fast.dpdk.org/rel/dpdk-1.8.0.tar.xz
-#tar -xvf dpdk-1.8.0.tar.xz
-#cd dpdk-1.8.0
-#make config T=x86_64-native-linuxapp-gcc && make
+# Step 1 - Set up Intel Data Plane Development Kit(DPDK) for network performance testing
+${MEGAKV_HOME}/bin/dpdk_setup.sh
 
-# https://github.com/att/vfd/wiki/Building-the-igb_uio-driver
-sudo yum install gcc git gmake kernel-devel -y
-mkdir $HOME/build
-cd $HOME/build
-git clone http://dpdk.org/git/dpdk
-cd dpdk
-# TODO: This is not v1.7.1 the MegaKV author recommended - will it work?
-git checkout v16.11
-make config T=x86_64-native-linuxapp-gcc
+# TODO: Steps 2 & 3 - Verify if this works from scratch, see README
+# TODO: ... e.g. CUDA installation path is?
+cd ${MEGAKV_HOME}/libgpuhash
+make
 
-# On the AWS P2 instance
-# Choose option 13 x86_64-native-linuxapp-gcc
-# (lots of messages/wait for 2-5 minutes)
-# Then choose option 16 (Insert IGB UIO module) and 33 (Exit Script)
-tools/dpdk-setup.sh
-# TODO: Installation cannot run with T defined and DESTDIR undefined
-# TODO: ... ^ Check if the above is an issue or not?
-
-# TODO: Takes 2-5 minutes? Might be a duplicate of the 'tools/dpdk-setup.sh' above...
-RTE_SDK=`pwd` make
-
-pwd
-# /home/ec2-user/build/dpdk
-
-#sudo make install T=x86_64-native-linuxapp-gcc
+# TODO: Step 4 - Same as previous steps - CUDA installation path?
+cd ${MEGAKV_HOME}/src
+RTE_SDK="$HOME/build/dpdk" make  # RTE_SDK value assumed to be true from Step 1
